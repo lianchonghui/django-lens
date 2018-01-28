de7eaa9fcd66adbf5ae8376650de44bec49f55a3
lianchonghui <lianchonghui@foxmail.com>
2018-01-28 13:07:52 +0800

    fix settings

--- lensproject/urls.py
+++ lensproject/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 
-from Django.views.generic.base import RedirectView
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
